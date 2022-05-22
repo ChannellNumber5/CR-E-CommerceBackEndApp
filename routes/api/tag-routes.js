e0b44779c8d66b3d7a65dce76f9113c3193c584c
Christina R <xsnachannell@gmail.com>
2022-05-21 21:50:51 -0700

    udpates tag-routes.js with RESTful CRUD operations

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,24 +4,65 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{model: Product, ProductTag}]
+    });
+    res.status(200).json(TagData);
+  } catch (err){
+      res.status(500).json(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
+  try{
+    const tagData = await Tag.findByPK(req.params.id, {
+      include: [{model: Product, ProductTag}]
+    });
+  } catch (err){
+      res.status(500).json(err);
+  }
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name:res.body.tag_name
+  })
+  .then((newTag) => res.status(200).json(newTag))
+  .catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+    // updates a tag's name by its `id` value, when given an id value as a parameter
+  Tag.update(
+    {
+      tag_name:req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
 });
 
 router.delete('/:id', (req, res) => {
+  // deletes tag with id identified in url parameter and then returns the deleted Tag information when tag was deleted successfully
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deletedTag) => {
+    res.json(deletedTag);
+  })
+  .catch((err) => res.json(err));
   // delete on tag by its `id` value
 });
 
